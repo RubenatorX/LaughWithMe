39afcfec1e77bcc93389e19adb07159de0fc0a8c
RubenatorX <RubenatorX@Gmail.com>
2013-12-07 23:39:17 -0700

    Login Works!

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,39 +1,14 @@
 from django.db import models
 from django.db.models import Manager
 from django.db.models.query import QuerySet
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
+from django.core.validators import RegexValidator
+
 '''
-# Create your models here.
-class UserData(models.Model):
-    email = models.CharField(max_length=254, validators=[validate_email])
-    screenname = models.CharField(max_length=25, unique=True)
-    created = models.DateTimeField(auto_now_add=True)
-    objects = CaseInsensitiveManager(['screenname','email'])
-class Posts(models.Model):
-    title = models.CharField(max_length=50, validators=[MinLengthValidator(1)])
-    text = models.TextField(validators=[MaxLengthValidator(2000)])
-    #image = None ###
-class Favorites(models.Model):
-    user = models.ForeignKey(Users) ##need to find out what Users actually should be called
-    favorite = models.ForeignKey(User) ##
-class Notifications(models.Model):
-    user = models.ForeignKey(Users) ##
-    activity = models.ForeignKey(Comments)
-class Comments(models.Model):
-    user = models.ForeignKey(Users) ##
-    text = models.TextField(validators=[MaxLengthValidator(500)])
-    ###type = 
-class Tags(models.Model):
-    tag = models.CharField(max_length=40, unique=True, validators=[MinLengthValidator(1),
-            RegexValidator(
-                r'^[a-zA-Z][a-zA-Z0-9]*',
-                'Must be a letter followed by letters and numbers.',
-                'Invalid Hashtag'
-            )])
-    objects = CaseInsensitiveManager(['tag'])
-
-
-
-
 #needs testing
 class CaseInsensitiveQuerySet(QuerySet, fieldnames):
     def _filter_or_exclude(self, mapper, *args, **kwargs):
@@ -44,8 +19,63 @@ class CaseInsensitiveQuerySet(QuerySet, fieldnames):
         return super(CaseInsensitiveQuerySet, self)._filter_or_exclude(mapper, *args, **kwargs)
 class CaseInsensitiveManager(Manager, fieldnames):
     def get_query_set(self):
-        return TagCaseInsensitiveQuerySet(self.model, fieldnames)
+        return CaseInsensitiveQuerySet(self.model, fieldnames)
 '''
+
+# Create your models here.
+class UserData(models.Model):
+    user = models.ForeignKey(User)
+    screenname = models.CharField(max_length=25, unique=True)
+    favorites = models.ManyToManyField('self', through='Favorite', 
+                                           symmetrical=False, 
+                                           related_name='userFavorites')
+    #objects = CaseInsensitiveManager(['screenname'])
+    def getFavorites(self):
+        return self.userFavorites.filter(
+            favoritee__favoriter=self)
+class Tag(models.Model):
+    tag = models.CharField(max_length=40, unique=True, validators=[MinLengthValidator(1),
+            RegexValidator(
+                r'^[a-zA-Z][a-zA-Z0-9]*',
+                'Must be a letter followed by letters and numbers.',
+                'Invalid Hashtag'
+            )])
+    #objects = CaseInsensitiveManager(['tag'])
+class Post(models.Model):
+    user = models.ForeignKey(UserData)
+    title = models.CharField(max_length=50, validators=[MinLengthValidator(1)])
+    text = models.TextField(validators=[MaxLengthValidator(2000)])
+    #image = None ###
+    tags = models.ManyToManyField(Tag)
+class Favorite(models.Model):
+    user = models.ForeignKey(UserData, related_name='favoriter')
+    favorite = models.ForeignKey(UserData, related_name='favoritee')
+    def addFavorite(self, target):
+        created = Favorite.objects.get_or_create(
+            user=self,
+            favorite=target)
+        #Sanity Check?
+        return created
+    def delFavorite(self, target):
+        toDelete = Favorite.objects.filter(
+            user=self,
+            favorite=target)
+        #Sanity Check?
+        toDelete.delete()
+        return
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    commenter = models.ForeignKey(UserData)
+    text = models.TextField(validators=[MaxLengthValidator(500)])
+    ###type = 
+class Notification(models.Model):
+    user = models.ForeignKey(UserData)
+    activity = models.ForeignKey(Comment)
+
+
+
+
+
 '''
 #example
 class Tags(models.Model):
--- mainsite/views.py
+++ mainsite/views.py
@@ -2,20 +2,52 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 
 
 def WelcomeView(request):
     if request.user.is_authenticated():
-        pass# Do something for authenticated users.
+        # Do something for authenticated users.
+        return render(request, 'mainsite/temploggedin.html', {'user':request.user})
     else:
         # Do something for anonymous users.
-        return render_to_response('mainsite/welcome.html')
+        form = LoginForm()
+        return render(request, 'mainsite/welcome.html',{'form': form})
 def AboutView(request, ignore):
-    return render_to_response('mainsite/about.html')
+    form = LoginForm()
+    return render(request, 'mainsite/about.html',{'form': form})
 def LoginView(request, ignore):
-    return render_to_response('mainsite/login.html')
+    if request.method == 'POST': # If the form has been submitted...
+        form = LoginForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            user = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    #Successful login
+                    login(request, user)
+                    return redirect('/')
+                else:
+                    # Account Disabled
+                    return render(request, 'mainsite/login.html', {
+                        'form': form,
+                        })
+        #bad login
+        loginError = "The username and/or password was incorrect. Please try again."
+        return render(request, 'mainsite/login.html', {
+                'form': form,
+                'loginError' : loginError,
+                })
+            
+    else:
+        form = LoginForm() # An unbound form
+
+    return render(request, 'mainsite/login.html', {
+        'form': form,
+    })
+
 
 '''def RegistrationView(request, ignore):
     return render_to_response('mainsite/registration.html')'''
@@ -40,8 +72,26 @@ def RegistrationView(request, ignore):
         'form': form,
     })
 
+class LoginForm(forms.Form):
+    email = forms.email = forms.EmailField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Email',
+                'class':'form-control',
+            }
+        )
+    )
+    password = forms.CharField(max_length=32,
+        widget=forms.PasswordInput(
+            render_value=True,
+            attrs={
+                'placeholder':'password',
+                'class':'form-control',
+            }
+        )
+    )
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(max_length=254, min_length=7,
+    email = forms.EmailField(max_length=30, min_length=7,
         widget=forms.TextInput(
             attrs={
                 'placeholder':'Email',
